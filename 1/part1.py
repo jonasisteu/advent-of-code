21e28e63a3c9d6f827278843a2d1604df3fa3e9d
Jonasi STEU <jonasisteu@gmail.com>
2025-12-03 21:41:23 +0100

    [ADD] -> Exercise 1

---
+++ 1/part1.py
@@ -0,0 +1,19 @@
+file = open('input.txt').readlines()
+
+dial = 50
+password = 0
+
+for instruction in file:
+    direction, distance = instruction[:1], int(instruction[1:])
+
+    if direction == 'L':
+        for _ in range(distance):
+            dial = (dial - 1) % 100
+    else:
+        for _ in range(distance):
+            dial = (dial + 1) % 100
+
+    if dial == 0:
+        password += 1
+
+print(password)
---
+++ 1/part2.py
@@ -0,0 +1,20 @@
+file = open('input.txt').readlines()
+
+dial = 50
+password = 0
+
+for instruction in file:
+    direction, distance = instruction[:1], int(instruction[1:])
+
+    if direction == 'L':
+        for _ in range(distance):
+            dial = (dial - 1) % 100
+            if dial == 0:
+                password += 1
+    else:
+        for _ in range(distance):
+            dial = (dial + 1) % 100
+            if dial == 0:
+                password += 1
+
+print(password)
